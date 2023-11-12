9273ec87eb4869b2309ebae62e4675499151b8cf
oksusu <113329728+oksusu8@users.noreply.github.com>
2023-11-12 21:34:16 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(393, 657);
+}
+
+function draw() {
+  background(0);
+  fill(250);
+  noStroke();
+  circle(196.5, 570, 80);
+  fill(255);
+  stroke(0);
+  strokeWeight(5);
+  circle(196.5, 570, 68);
+  
+  
+  fill(220);
+  noStroke();
+  rect(0,50,393,433);
+}
