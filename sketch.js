e175e519ae12fbd93f024b5abe01ca2c9cddadf3
oksusu <113329728+oksusu8@users.noreply.github.com>
2023-11-13 15:41:20 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,29 @@
 function setup() {
-  createCanvas(393, 657);
+  createCanvas(windowWidth, windowHeight); //createCanvas(393, 657);
 }
+
+
 
 function draw() {
   background(0);
-  fill(250);
-  noStroke();
-  circle(196.5, 570, 80);
-  fill(255);
-  stroke(0);
-  strokeWeight(5);
-  circle(196.5, 570, 68);
-  
   
   fill(220);
   noStroke();
-  rect(0,50,393,433);
+  rect(0, windowHeight*0.08, windowWidth, windowHeight*0.7)
+  //rect(0,50,393,433); // x좌표, y좌표, width, height
+  
+  
+  
+  fill(250);
+  noStroke();
+  circle(windowWidth/2, windowHeight*0.86, 80); // 중심x좌표, y좌표, 지름
+  
+  
+  
+  fill(255);
+  stroke(0);
+  strokeWeight(5);
+  //circle(196.5, 570, 68);
+  
+  
 }
