524130303a0f35113a7b5d3c3fd25a88875525b6
oksusu <113329728+oksusu8@users.noreply.github.com>
2023-11-16 11:13:53 +0900

    5th commit

--- sketch.js
+++ sketch.js
@@ -1,29 +1,27 @@
+let video;
+
 function setup() {
   createCanvas(windowWidth, windowHeight); //createCanvas(393, 657);
+  video = createCapture(VIDEO);
+  video.size(windowWidth, windowHeight*0.7); //video.size(width, height);
 }
 
 
 
 function draw() {
   background(0);
-  
-  fill(220);
-  noStroke();
-  rect(0, windowHeight*0.08, windowWidth, windowHeight*0.7)
-  //rect(0,50,393,433); // x좌표, y좌표, width, height
-  
-  
+  image(video, 0, windowHeight*0.08, windowWidth, windowHeight*0.7);
   
   fill(250);
   noStroke();
-  circle(windowWidth/2, windowHeight*0.87, windowWidth*0.23); // 중심x좌표, y좌표, 지름
+  circle(windowWidth/2, windowHeight*0.87, windowWidth*0.13); // 중심x좌표, y좌표, 지름
   
   
   
   fill(255);
   stroke(0);
   strokeWeight(3);
-  circle(windowWidth/2, windowHeight*0.87, windowWidth*0.21); // 중심x좌표, y좌표, 지름
+  circle(windowWidth/2, windowHeight*0.87, windowWidth*0.11); // 중심x좌표, y좌표, 지름
   
   //circle(196.5, 570, 68);
   
