02f4df83885a5e3b2b7315f5da1d5925cddb4c08
oksusu <113329728+oksusu8@users.noreply.github.com>
2023-11-17 03:10:50 +0900

    6th commit

--- sketch.js
+++ sketch.js
@@ -1,29 +1,26 @@
 let video;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight); //createCanvas(393, 657);
-  video = createCapture(VIDEO);
-  video.size(windowWidth, windowHeight*0.7); //video.size(width, height);
+    createCanvas(windowWidth, windowHeight);
+    video = createCapture(VIDEO);
+    video.size(windowWidth, windowHeight*0.7);
+
+    video.hide();
 }
 
-
-
 function draw() {
-  background(0);
-  image(video, 0, windowHeight*0.08, windowWidth, windowHeight*0.7);
-  
-  fill(250);
-  noStroke();
-  circle(windowWidth/2, windowHeight*0.87, windowWidth*0.13); // 중심x좌표, y좌표, 지름
-  
-  
-  
-  fill(255);
-  stroke(0);
-  strokeWeight(3);
-  circle(windowWidth/2, windowHeight*0.87, windowWidth*0.11); // 중심x좌표, y좌표, 지름
-  
-  //circle(196.5, 570, 68);
-  
-  
+    background(0);
+    // 카메라
+    image(video, 0, windowHeight*0.08, windowWidth, windowHeight*0.7);
+    
+    // 촬영 버튼
+    fill(250);
+    noStroke();
+    circle(windowWidth/2, windowHeight*0.87, windowWidth*0.13);
+    
+    fill(255);
+    stroke(0);
+    strokeWeight(3);
+    circle(windowWidth/2, windowHeight*0.87, windowWidth*0.11);
+
 }
