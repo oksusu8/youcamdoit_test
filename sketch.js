4eecc9214e7a0c06f1e455d1e46cb4000d0e4a5a
oksusu <113329728+oksusu8@users.noreply.github.com>
2023-11-27 14:43:20 +0900

    12th

--- sketch.js
+++ sketch.js
@@ -1,12 +1,57 @@
+let buttonX, buttonY;
+let buttonWidth = 100;
+let buttonHeight = 40;
+
 function setup() {
-  createCanvas(393, 852);
+  createCanvas(windowWidth, windowHeight);
+  buttonX = width / 2;
+  buttonY = height / 2;
   
-  auto_btn = createButton('Auto');
-  auto_btn.position(width/2, 0);
+  // rectMode 함수를 사용하여 사각형 모드를 CENTER로 설정
+  rectMode(CENTER);
+  background(160);
+  
+  // 버튼을 중심에 그리기
+  drawButton();
+  autobtn_draw();
 }
 
 function draw() {
-  background(170);
+  // 화면을 그리는 다른 코드
+}
+
+
+function autobtn_draw() {
+  fill(255);
+  rect(buttonX, 0+buttonHeight/2, buttonWidth, buttonHeight);
   
+  fill(0);
+  textAlign(CENTER, CENTER);
+  textSize(16);
+  text("Auto", buttonX, 0+buttonHeight/2);
+}
+
+function drawButton() {
+  // 버튼 그리기
+  fill(50, 150, 200);
+  rect(buttonX, buttonY, buttonWidth, buttonHeight);
   
+  // 버튼 텍스트 그리기
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textSize(16);
+  text("버튼", buttonX, buttonY);
+}
+
+function mousePressed() {
+  // 마우스 클릭이 버튼 내에 있는지 확인
+  if (
+    mouseX > buttonX - buttonWidth / 2 &&
+    mouseX < buttonX + buttonWidth / 2 &&
+    mouseY > buttonY - buttonHeight / 2 &&
+    mouseY < buttonY + buttonHeight / 2
+  ) {
+    // 버튼이 클릭되면 동작 수행
+    console.log("버튼이 클릭되었습니다!");
+  }
 }
